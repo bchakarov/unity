b4040787129e7b7667c57c2a9e7af88ee6484b25
bchakarov <40578025+bchakarov@users.noreply.github.com>
2020-10-15 14:45:58 +0300

    Add flappy bird game

---
+++ FlappyBird/Assets/Scripts/BackgroundCollideController.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+
+public class BackgroundCollideController : MonoBehaviour
+{
+    private int numberOfBackgrouds;
+    private float distanceBetweenBackgrounds;
+
+    private int numberOfGrounds;
+    private float distanceBetweenGrounds;
+
+    private int numberOfPipes;
+    private float distanceBetweenPipes;
+
+    private bool upperPipe;
+
+    public void Start()
+    {
+        var backgrounds = GameObject.FindGameObjectsWithTag("Background");
+        var grounds = GameObject.FindGameObjectsWithTag("Ground");
+        var pipes = GameObject.FindGameObjectsWithTag("Pipe");
+        
+        this.RandomizePipes(pipes);
+
+        this.numberOfBackgrouds = backgrounds.Length;
+        this.numberOfGrounds = grounds.Length;
+        this.numberOfPipes = pipes.Length;
+
+        if (this.numberOfBackgrouds < 2 || this.numberOfGrounds < 2 || this.numberOfPipes < 2)
+        {
+            throw new System.InvalidOperationException("You must have at least two backgrounds or grounds or pipes in your scene!");
+        }
+
+        this.distanceBetweenBackgrounds = this.DistanceBetweenObjects(backgrounds[0], backgrounds[1]);
+        this.distanceBetweenGrounds = this.DistanceBetweenObjects(grounds[0], grounds[1]);
+        this.distanceBetweenPipes = this.DistanceBetweenObjects(pipes[0], pipes[1]);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Background") 
+            || collider.CompareTag("Ground") 
+            || collider.CompareTag("Pipe"))
+        {
+            var go = collider.gameObject;
+            var originalPosition = go.transform.position;
+            
+            if (collider.CompareTag("Background"))
+            {
+                originalPosition.x += this.numberOfBackgrouds * this.distanceBetweenBackgrounds;
+            }
+            else if (collider.CompareTag("Ground"))
+            {
+                originalPosition.x += this.numberOfGrounds * this.distanceBetweenGrounds;
+            }
+            else
+            {
+                originalPosition.x += this.numberOfPipes * this.distanceBetweenPipes;
+
+                var randomY = 0f;
+                if (this.upperPipe)
+                {
+                    // generate upper pipe
+                    randomY = Random.Range(0, 2);
+                }
+                else
+                {
+                    // generate down pipe
+                    randomY = Random.Range(-2, 0);
+                }
+
+                originalPosition.y = randomY;
+
+                this.upperPipe = !this.upperPipe;
+            }
+
+            go.transform.position = originalPosition;
+        }
+    }
+
+    private float DistanceBetweenObjects(GameObject first, GameObject second)
+    {
+        return Mathf.Abs(first.transform.position.x - second.transform.position.x);
+    }
+
+    private void RandomizePipes(GameObject[] pipes)
+    {
+        int count = 0;
+
+        for (int i = 1; i < pipes.Length; i++)
+        {
+            count++;
+            var currentPipe = pipes[i];
+            var randomY = 0f;
+            if (count % 2 == 0) // upper pipe
+            {
+                randomY = Random.Range(0, 2);
+            }
+            else // down pipe
+            {
+                randomY = Random.Range(-2, 0);
+            }
+
+            var pipePosition = currentPipe.transform.position;
+            pipePosition.y = randomY;
+            currentPipe.transform.position = pipePosition;
+        }
+    }
+}
---
+++ FlappyBird/Assets/Scripts/BirdController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BirdController : MonoBehaviour
+{
+    public float flapSpeed = 1000f;
+    public float forwardSpeed = 10f;
+    public float maxFlapSpeed = 2f;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    private bool didFlap;
+    private bool isDead;
+
+    private bool gameStarted;
+
+    private Vector2 originalPosition;
+
+    private GameObject startButton;
+
+    private int score = 0;
+
+    public void Start()
+    {
+        Debug.Log(PlayerPrefs.GetInt("HighScore"));
+        this.rb = this.GetComponent<Rigidbody2D>();
+        this.animator = this.GetComponent<Animator>();
+        this.originalPosition = new Vector2(this.transform.position.x, this.transform.position.y);
+        this.startButton = GameObject.Find("StartButton");
+
+        this.rb.gravityScale = 0;
+        this.forwardSpeed = 0;
+        this.animator.enabled = false;
+    }
+
+    // read input, change graphics
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (!isDead)
+            {
+                if (!this.gameStarted)
+                {
+                    var renderer = this.startButton.GetComponent<SpriteRenderer>();
+                    renderer.enabled = false;
+                    this.forwardSpeed = 5;
+                    this.rb.gravityScale = 1;
+                    this.animator.enabled = true;
+                }
+
+                this.didFlap = true;
+            }
+            else
+            {
+                SceneManager.LoadScene("Play");
+            }
+        }
+    }
+
+    // apply physics
+
+    public void FixedUpdate()
+    {
+        var velocity = this.rb.velocity;
+        velocity.x = this.forwardSpeed;
+        this.rb.velocity = velocity;
+
+        var rotationAngle = (this.rb.velocity.y > 0) ? Math.Min(rb.velocity.y * 8, 30)
+            : Math.Max(rb.velocity.y * 8, -90);
+
+        if (!isDead)
+        {
+            this.rb.MoveRotation(rotationAngle);
+        }
+
+        if (didFlap)
+        {
+            didFlap = false;
+            this.rb.AddForce(new Vector2(0, flapSpeed));
+
+            var updatedVelocity = this.rb.velocity;
+            if (updatedVelocity.y > this.maxFlapSpeed)
+            {
+                updatedVelocity.y = this.maxFlapSpeed;
+                this.rb.velocity = updatedVelocity;
+            }
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.CompareTag("Floor") || collider.gameObject.CompareTag("PipeCollision"))
+        {
+            this.isDead = true;
+            this.animator.SetBool("BirdDead", true);
+            this.forwardSpeed = 0;
+
+            var currentHighScore = PlayerPrefs.GetInt("HighScore", 0);
+            if (this.score > currentHighScore)
+            {
+                PlayerPrefs.SetInt("HighScore", this.score);
+            }
+
+            var renderer = this.startButton.GetComponent<SpriteRenderer>();
+            this.startButton.transform.position = new Vector3(Camera.main.transform.position.x,
+                Camera.main.transform.position.y, 0);
+            renderer.enabled = true;
+
+            this.gameStarted = false;
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Pipe"))
+        {
+            this.score++;
+            var text = GameObject.Find("Text");
+            text.GetComponent<Text>().text = score.ToString();
+        }
+    }
+}
